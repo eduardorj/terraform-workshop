0f358b1078fcdb9dc3306e459845f1dc610a3453
Eduardo Francellino <eduardo@aproveita.com.br>
2021-03-31 15:45:42 -0300

    refactoring

--- src/main.tf
+++ src/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    ibm = {
+      source  = "localdomain/provider/ibm"
+      version = "1.10.0"
+    }
+  }
+}
+
 resource "tls_private_key" "ssh" {
   algorithm = "RSA"
 }
@@ -7,22 +16,24 @@ resource "ibm_compute_ssh_key" "temp_public_key" {
   public_key = "${tls_private_key.ssh.public_key_openssh}"
 }
 
-resource "ibm_compute_vm_instance" "softlayer_virtual_guest" {
+# Create a new virtual guest using image "Debian"
+resource "ibm_compute_vm_instance" "debian_small_virtual_guest" {
   hostname                 = "${var.hostname}"
-  os_reference_code        = "UBUNTU_18_64"
+  os_reference_code        = "DEBIAN_9_64"
   domain                   = "ed.blog.br"
   datacenter               = "wdc01"
   network_speed            = 10
   hourly_billing           = true
   private_network_only     = false
-  cores                    = 2
-  memory                   = 2048
-  disks                    = [25]
+  cores                    = 1
+  memory                   = 1024
+  disks                    = [25, 10, 20]
+  user_metadata            = "{\"value\":\"newvalue\"}"
   dedicated_acct_host_only = false
   local_disk               = false
   ssh_key_ids              = ["${ibm_compute_ssh_key.temp_public_key.id}"]
 
-  # Specify the ssh connection
+ # Specify the ssh connection
   connection {
     user        = "root"
     private_key = "${tls_private_key.ssh.private_key_pem}"
@@ -33,15 +44,15 @@ resource "ibm_compute_vm_instance" "softlayer_virtual_guest" {
     inline = [
       "echo \"root:${var.password}\" | chpasswd",
       "apt-get update",
-      "apt-get install python ansible -y",
+      "apt-get install python ansible git -y",
       "echo \"[apache]\" >> /etc/ansible/hosts",
       "echo $(/sbin/ip -o -4 addr list eth1 | awk '{print $4}' | cut -d/ -f1) >> /etc/ansible/hosts",
-      "git clone https://github.com/eduardorj/ansible-apache.git",
-      "ansible-playbook ./ansible-apache/apache.yaml --connection=local",
+      "git clone https://github.com/eduardorj/ansible-playbooks.git",
+      "ansible-playbook ./ansible-playbooks/apache.yaml --connection=local",
     ]
   }
 }
 
-output "ip" {
-  value = "${ibm_compute_vm_instance.softlayer_virtual_guest.ipv4_address}"
+output "vm_ip" {
+  value = "Public : ${ibm_compute_vm_instance.debian_small_virtual_guest.ipv4_address}"
 }
